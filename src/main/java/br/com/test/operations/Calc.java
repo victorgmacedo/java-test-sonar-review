5324b2947bcf9b4f1e1c988f9765e612edba7496
Victor Gabriel <victormacedo5874@gmail.com>
2021-04-21 00:30:45 -0300

    Commit

---
+++ src/main/java/br/com/test/operations/Calc.java
@@ -0,0 +1,13 @@
+package br.com.test.operations;
+
+import java.util.stream.Stream;
+
+public class Calc {
+
+	public Integer calcular(Integer... values) {
+		return Stream.of(values).reduce(0, (valor, acc) -> {
+			return valor += acc;
+		});
+	}
+	
+}
---
+++ src/test/java/br/com/test/JavaTestApplicationTests.java
@@ -0,0 +1,25 @@
+package br.com.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import br.com.test.operations.Calc;
+
+class JavaTestApplicationTests {
+	
+	private Calc calc;
+	
+	@BeforeEach
+	public void setup() {
+		calc = new Calc();
+	}
+
+	@Test
+	void first_test() {
+		Integer total = calc.calcular(1,3,4);
+		Assertions.assertEquals(8, total);
+	}
+
+}
